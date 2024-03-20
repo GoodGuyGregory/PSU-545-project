91c3c8c142ed974c0a23ec9381412122e85e5cff
Tim Rice <whoneedsgravity@gmail.com>
2024-03-20 09:11:51 -0700

    kmeans updates

--- tim/kmeans.py
+++ tim/kmeans.py
@@ -1,2 +1,17 @@
 import numpy as np
-cd 
+import pandas as pd
+from sklearn.cluster import KMeans
+
+data = pd.read_csv('../data_no_labels.csv')
+
+# Assuming that 'data' is your dataset
+kmeans = KMeans(n_clusters=3)  # Specify the number of clusters
+kmeans.fit(data)
+
+# Getting the cluster labels
+labels = kmeans.predict(data)
+
+# Getting the cluster centers
+centers = kmeans.cluster_centers_
+
+print(centers)
