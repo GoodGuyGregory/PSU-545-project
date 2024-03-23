7ea42c5897419219fc07a0b69c594d2bfdbef29e
GoodGuyGregory <greg.witt625@gmail.com>
2024-03-22 22:53:17 -0400

    adds decision tree classification

--- greg/Decision_Tree/main.py
+++ greg/Decision_Tree/main.py
@@ -1,7 +1,17 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+
+def getBloodFeatures():
+    df = pd.read_csv('Blood_samples_dataset_balanced_2(f).csv')
+    df = df.drop('Disease', axis=1)
+    featureNames = df.columns.tolist()
+    return featureNames
 
 def loadTrainingData():
     trainingData = pd.read_csv('Blood_samples_dataset_balanced_2(f).csv')
@@ -16,6 +26,7 @@ def loadTrainingData():
 
 def loadTestingData():
     testingData = pd.read_csv('blood_samples_dataset_test.csv')
+    testingData = testingData[testingData['Disease'] != 'Heart Di']
     # shuffle the training data
     testingData.sample(frac=1)
     # replace the labels as numeric values
@@ -26,6 +37,60 @@ def loadTestingData():
     return testDataFeatures, testingLabels
 
 def main():
+    blood_features = getBloodFeatures()
+    trainingData, trainingLabels = loadTrainingData()
+    testingData, testingLabels = loadTestingData()
 
+    # 70 % Training 30 % test
+    X_train, X_test, y_train, y_test = train_test_split(trainingData, trainingLabels, test_size=0.3, random_state=42)
+
+    treeClassifier = DecisionTreeClassifier(max_depth=4)
+    # fit the decision model
+    treeClassifier.fit(X_train, y_train)
+    #  run classifier on the X test data
+    y_predictions = treeClassifier.predict(X_test)
+
+
+#      Training Data:
+#    initial evaluation
+    print("Accuracy:", metrics.accuracy_score(y_test, y_predictions))
+
+    # Calculate the confusion matrix
+    cm = confusion_matrix(y_test, y_predictions)
+
+    # Plot the confusion matrix using seaborn and matplotlib
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, cmap='Greens', fmt='g')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('Actual labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+    plt.figure(figsize=(75, 30))
+    plot_tree(treeClassifier, fontsize=15, filled=True, feature_names=blood_features, class_names=['Healthy', 'Diabetes', 'Thalasse', 'Anemia', 'Thromboc'])
+    plt.show()
+
+#    Testing Data
+    y_testingPredicitons = treeClassifier.predict(testingData)
+
+    #      Training Data:
+    #    initial evaluation
+    print("Accuracy:", metrics.accuracy_score(testingLabels, y_testingPredicitons))
+
+    # Calculate the confusion matrix
+    cm = confusion_matrix(testingLabels, y_testingPredicitons)
+
+    # Plot the confusion matrix using seaborn and matplotlib
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(cm, annot=True, cmap='Greens', fmt='g')
+    plt.xlabel('Predicted labels')
+    plt.ylabel('Actual labels')
+    plt.title('Confusion Matrix')
+    plt.show()
+
+    plt.figure(figsize=(75, 30))
+    plot_tree(treeClassifier, fontsize=15, filled=True, feature_names=blood_features,
+              class_names=['Healthy', 'Diabetes', 'Thalasse', 'Anemia', 'Thromboc'])
+    plt.show()
 
 main()
